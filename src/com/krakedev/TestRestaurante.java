cdc57e9633ebc0e292f2e45f7e44eb634c1b5ba2
JonathanLuna95 <jjlunap95@gmail.com>
2026-08-02 16:38:00 -0500

    Agregar constructor parametrizado en Restaurante

--- src/com/krakedev/TestRestaurante.java
+++ src/com/krakedev/TestRestaurante.java
@@ -6,34 +6,17 @@ public class TestRestaurante {
 		// TODO Auto-generated method stub
 		
 		Restaurante rest1;
-		rest1 = new Restaurante();
+		rest1 = new Restaurante("Trio Cocina Libre", "Av. Río Lelia", 4.8f);
 		
-		Restaurante rest2 = new Restaurante();
+		Restaurante rest2 = new Restaurante("Origen", "Av. La Lorena", 4.5f);
 		
-		System.out.println("**********Restaurante 1 con valores por defecto************");
+
+		System.out.println("**********Restaurante 1 ************");
 		System.out.println("Nombre : " + rest1.getNombre());
 		System.out.println("Direccion : " + rest1.getDireccion());
 		System.out.println("Calificacion : " + rest1.getCalificacion());
 		
-		System.out.println("**********Restaurante 2 con valores por defecto************");
-		System.out.println("Nombre : " + rest2.getNombre());
-		System.out.println("Direccion : " + rest2.getDireccion());
-		System.out.println("Calificacion : " + rest2.getCalificacion());
-		
-		rest1.setNombre("Trio Cocina Libre");
-		rest1.setDireccion("Av. Río Lelia");
-		rest1.setCalificacion(4.8f);
-
-		rest2.setNombre("Origen"); 
-		rest2.setDireccion("Av. La Lorena");
-		rest2.setCalificacion(4.5f);
-
-		System.out.println("**********Restaurante 1 con valores por defecto************");
-		System.out.println("Nombre : " + rest1.getNombre());
-		System.out.println("Direccion : " + rest1.getDireccion());
-		System.out.println("Calificacion : " + rest1.getCalificacion());
-		
-		System.out.println("**********Restaurante 2 con valores por defecto************");
+		System.out.println("**********Restaurante 2 ************");
 		System.out.println("Nombre : " + rest2.getNombre());
 		System.out.println("Direccion : " + rest2.getDireccion());
 		System.out.println("Calificacion : " + rest2.getCalificacion());
